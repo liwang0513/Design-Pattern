22b7c448b9a300dcb261669fdf1e8dda58866df7
liwang0513 <liwang0513@gmail.com>
2015-01-19 19:34:56 -0500

    strategy
    define a family algorithms, encapsulate each one, and make them
    interchangeable. The Strategy pattern lets the algorithm vary
    independently from clients that use it.

---
+++ src/strategy/Flys.java
@@ -0,0 +1,25 @@
+package strategy;
+
+public interface Flys {
+	
+	String fly();
+	
+}
+
+class ItFlys implements Flys {
+
+	@Override
+	public String fly() {
+		return "Flying high";
+	}
+	
+}
+
+class CantFly implements Flys {
+
+	@Override
+	public String fly() {
+		return "I can't fly";
+	}
+	
+}
